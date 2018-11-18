dd14dec282d345200a2c5ff6042f5166d57357ef
EliasRut <eliasrut@gmx.at>
2018-11-18 13:55:42 +0100

    Added functionality for stop-play functionality for quizes.

--- src/index.py
+++ src/index.py
@@ -23,14 +23,17 @@ for line in labyrinth_lines:
     gems.append(gem_row)
 
 # Pyglet window and background
-window = pyglet.window.Window(fullscreen = True)
+window = pyglet.window.Window(fullscreen = False)
 background = pyglet.resource.image('background.png')
+
+window_width = int(window.width)
+window_height = int(window.height)
 
 # Batch layer for all the stones and gem drawings
 batch = pyglet.graphics.Batch()
 
 # Size definition for a single square
-unit_size = window.height/22
+unit_size = window_height/22
 
 # Wall image
 stone_image = pyglet.image.load('stone.png')
@@ -58,34 +61,42 @@ gems_gained = []
 
 # Image quiz definitions
 quizes = []
+wait_for_space = []
 
-def add_quiz(image_files):
+def add_quiz(wait, image_files):
     global quizes
     quiz = []
+    wait_for_space.append(wait)
     for image in image_files:
         quiz.append(pyglet.resource.image(image))
     quizes.append(quiz)
 
 # Startbild
-add_quiz(['start.jpg'])
+add_quiz([], ['start.jpg'])
 # Erstes Quiz
-add_quiz(['Hibiskus_1.JPG','Hibiskus_2.JPG','Hibiskus_3.JPG','Hibiskus_4.JPG','Hibiskus_5.JPG','Hibiskus_6.JPG','Hibiskus_7.JPG','Frauenschuh_1.jpg','Frauenschuh_2.jpg','Frauenschuh_3.jpg','Frauenschuh_4.jpg','Frauenschuh_5.jpg','Frauenschuh_6.jpg','Frauenschuh_7.jpg'])
+add_quiz([5,6,12,13], [
+    'Hibiskus_1.JPG','Hibiskus_2.JPG','Hibiskus_3.JPG','Hibiskus_4.JPG','Hibiskus_5.JPG','Hibiskus_6.JPG','Hibiskus_7.JPG',
+    'Frauenschuh_1.jpg','Frauenschuh_2.jpg','Frauenschuh_3.jpg','Frauenschuh_4.jpg','Frauenschuh_5.jpg','Frauenschuh_6.jpg','Frauenschuh_7.jpg'
+])
 # Zweites Quiz
-add_quiz(['Ehe_2.jpg','Familie.jpg'])
+add_quiz([], ['Ehe_2.jpg','Familie.jpg'])
 # Drittes Quiz
-add_quiz(['Mauerbau.jpeg'])
+add_quiz([], ['Mauerbau.jpeg'])
 # Viertes Quiz
-add_quiz(['Freunde_1.jpg'])
+add_quiz([], ['Freunde_1.jpg'])
 # Fuenftes Quiz
-add_quiz(['Fortfahren_1.jpg'])
+add_quiz([], ['Fortfahren_1.jpg'])
 # Sechstes Quiz
-add_quiz(['Geschwister_1.jpg','Geschwister_2.jpg'])
+add_quiz([], ['Geschwister_1.jpg','Geschwister_2.jpg'])
 # Siebtes Quiz
-add_quiz(['Motorrad.jpg'])
+add_quiz([], ['Motorrad.jpg'])
 # Achtes Quiz
-add_quiz(['Nigeria.jpg'])
+add_quiz([], ['Nigeria.jpg'])
+
+IMAGE_DISPLAY_DURATION = 2.0
 
 image_index = 0
+image_time_remaining = IMAGE_DISPLAY_DURATION
 quiz_index = 0
 
 show_quiz = True
@@ -93,17 +104,22 @@ show_quiz = True
 
 def update_game(delta_time):
     global image_index
+    global image_time_remaining
+
     if not show_quiz:
         return;
-    if image_index < len(quizes[quiz_index])-1:
-        image_index += 1
+    if image_index < len(quizes[quiz_index])-1 and not image_index in wait_for_space[quiz_index]:
+        image_time_remaining -= delta_time
+        if image_time_remaining <= 0.0:
+            image_index += 1
+            image_time_remaining = IMAGE_DISPLAY_DURATION
 
 def update_stones():
     global stone_sprites
     stone_sprites = []
     for row in range(21):
         y = (row+1) * unit_size
-        for column in range(window.width/unit_size):
+        for column in range(int(window_width/unit_size)):
             x = column * unit_size
             if column + labyrinth_shift >= len(labyrinth[row]):
                 continue
@@ -121,7 +137,7 @@ def update_stones():
     for gem_type in gems_gained:
         gem = gems_seq[gem_type]
         gem_index += 1
-        stone_sprite = pyglet.sprite.Sprite(gem, window.width - (gem_index * unit_size), 0, batch=batch)
+        stone_sprite = pyglet.sprite.Sprite(gem, window_width - (gem_index * unit_size), 0, batch=batch)
         stone_sprite.scale = unit_size / stone_image_size
         stone_sprites.append(stone_sprite)
 
@@ -163,6 +179,7 @@ def on_key_press(symbol, modifiers):
     global show_quiz
     global player_position_x
     global player_position_y
+    global image_index
     if not show_quiz:
         if symbol == key.LEFT and is_valid(-1, 0):
             player_position_x -= 1
@@ -172,8 +189,11 @@ def on_key_press(symbol, modifiers):
             player_position_y += 1
         elif symbol == key.DOWN and is_valid(0, -1):
             player_position_y -= 1
-    elif show_quiz and symbol == key.ENTER:
-        show_quiz = False
+    elif show_quiz and symbol == key.SPACE:
+        if image_index == len(quizes[quiz_index]) - 1:
+            show_quiz = False
+        elif image_index in wait_for_space[quiz_index]:
+            image_index += 1
 
 
 @window.event
@@ -181,9 +201,9 @@ def on_draw():
     window.clear()
     check_for_gems()
     if show_quiz:
-        quizes[quiz_index][image_index].blit(0,0, width=window.width, height=window.height)
+        quizes[quiz_index][image_index].blit(0,0, width=window_width, height=window_height)
     else:
-        background.blit(0, unit_size, width=window.width, height=window.height-unit_size)
+        background.blit(0, unit_size, width=window_width, height=window_height-unit_size)
 
         update_stones()
         batch.draw()
@@ -191,5 +211,5 @@ def on_draw():
         draw_player()
         player.draw()
 
-pyglet.clock.schedule_interval(update_game, 2)
+pyglet.clock.schedule_interval(update_game, 1.0/120.0)
 pyglet.app.run()
